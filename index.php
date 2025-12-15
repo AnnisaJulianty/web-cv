f1a4337471e6410fa2040513b3cbe38e0c1cd437
Annisa <annisajuliantyid@gmail.com>
2025-12-15 11:48:45 +0700

    nama

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ $home_data['about_me'] = $home_data['about_me'] ?? 'Saya seorang mahasiswi siste
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About Me - Annisa Julianty</title>
+    <title>About Me - M Faiz Abdillah</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome (untuk ikon) -->
